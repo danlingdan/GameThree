d7bbc5257017350f95dfb7eb75cb0f041577415c
HuaJH <2844344532@qq.com>
2025-11-15 19:51:30 +0800

    19 瓦片层单向平台与斜坡

--- GameThree/src/engine/scene/level_loader.cpp
+++ GameThree/src/engine/scene/level_loader.cpp
@@ -267,6 +267,35 @@ namespace engine::scene {
                     auto is_solid = property.value("value", false);
                     return is_solid ? engine::component::TileType::SOLID : engine::component::TileType::NORMAL;
                 }
+                else if (property.contains("name") && property["name"] == "slope") {
+                    auto slope_type = property.value("value", "");
+                    if (slope_type == "0_1") {
+                        return engine::component::TileType::SLOPE_0_1;
+                    }
+                    else if (slope_type == "1_0") {
+                        return engine::component::TileType::SLOPE_1_0;
+                    }
+                    else if (slope_type == "0_2") {
+                        return engine::component::TileType::SLOPE_0_2;
+                    }
+                    else if (slope_type == "2_0") {
+                        return engine::component::TileType::SLOPE_2_0;
+                    }
+                    else if (slope_type == "2_1") {
+                        return engine::component::TileType::SLOPE_2_1;
+                    }
+                    else if (slope_type == "1_2") {
+                        return engine::component::TileType::SLOPE_1_2;
+                    }
+                    else {
+                        spdlog::error("未知的斜坡类型: {}", slope_type);
+                        return engine::component::TileType::NORMAL;
+                    }
+                }
+                else if (property.contains("name") && property["name"] == "unisolid") {
+                    auto is_unisolid = property.value("value", false);
+                    return is_unisolid ? engine::component::TileType::UNISOLID : engine::component::TileType::NORMAL;
+                }
                 // TODO: 可以在这里添加更多的自定义属性处理逻辑
             }
         }
